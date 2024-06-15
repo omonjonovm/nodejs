06e50428d338ec889bc45f9844743f3ca7ad1e99
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-15 16:28:46 +0500

    operation system

---
+++ modules/os.js
@@ -0,0 +1,18 @@
+const os = require('os')
+//platform 
+// console.log(os.platform());
+
+// information
+// console.log(os.cpus());
+
+// archhitect
+// console.log(os.arch());
+
+// free memory
+// console.log(os.freemem());
+
+// total memory
+// console.log(os.totalmem());
+
+// entry point
+// console.log(os.homedir());
