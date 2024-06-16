04ade719fe6b4c812488b1dc5cb27a567cb988ab
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-16 22:32:24 +0500

    Nodemon

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const server = http.createServer((request,response) => {
   // request - serverga so'rov 
   // response - server javobi
   console.log(request.url);
-  response.write('<h1>Hello World</h1>')
+  response.write('<h1>Hello World 3</h1>')
+  response.write('<h1>Hello World 17</h1>')
   response.end()
 })
 server.listen(3000,() => {
