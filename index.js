b56896d160f28ca449a84be096f1eed868b86268
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-17 22:29:51 +0500

    web server

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const http = require('http')
-const server = http.createServer((request,response) => {
-  // request - serverga so'rov 
-  // response - server javobi
-  console.log(request.url);
-  response.write('<h1>Hello World 3</h1>')
-  response.write('<h1>Hello World 17</h1>')
-  response.end()
+const server = http.createServer((req,res) => {
+      if(req.method === 'GET') {
+        res.writeHead(200,{"Content-Type": 'text/html'})
+        res.end(`
+          <h1>send name</h1>
+            <form method="post" action="/">
+              <input name="name"  placeholder="Enter your name" />
+              <button type="submit">Send name</button>
+            </form>
+          `)
+      } else if(req.method === 'POST' ) {
+        const name = []
+
+        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'})
+
+        req.on('data',data => {
+          name.push(Buffer.from(data))
+        })
+          req.on("end",() => {
+          const message = name.toString().split('=')[1]
+          
+          res.end(`name succesfully added: ${message}`)
+        })
+      }
 })
 server.listen(3000,() => {
   console.log('started on port:3000');
